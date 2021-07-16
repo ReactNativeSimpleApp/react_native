30e8a6325d48fe325a2e8126fd7b64c5985c8b17
Роман Худобляк <49787417+RKDGT@users.noreply.github.com>
2021-07-16 16:23:54 +0300

    basic request to users table

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import type {Node} from 'react';
-import hotelsService from './src/services/jsonApiServise';
 import AppStack from './src/components/stacks/AppStack';
 
 const App: () => Node = () => {
-  console.log(hotelsService.getUsers());
   return (
     <NavigationContainer>
       <AppStack />
--- src/components/screens/SignUp.js
+++ src/components/screens/SignUp.js
@@ -1,117 +1,138 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
+import jsonApiService from '../../services/jsonApiServise';
 
 import Text from '../reusable/Text';
 
 export default SignUp = ({navigation}) => {
-    return (
-        <Container>
-            <Main>
-                <Text title semi center>Sign Up</Text>
-            </Main>
+  let {email, setEmail} = useState();
+  let {users, setUsers} = useState();
+  useEffect(() => {
+    jsonApiService
+      .getUsers()
+      .then(data => {
+        setUsers(data.data);
+      })
+      .catch(err => {
+        console.log('fuck');
+      });
+  }, []);
 
-                <Auth>
-                    <AuthContainer>
-                        <AuthTitle>Full Name</AuthTitle>
+  useEffect(() => {}, [email]);
 
-                        <AuthField 
-                            autoCapitalize="none"
-                            autoCorrect={false}
-                            autoFocus={true}
-                            />
-                    </AuthContainer>
+  return (
+    <Container>
+      <Main>
+        <Text title semi center>
+          Sign Up
+        </Text>
+      </Main>
 
-                    <AuthContainer>
-                        <AuthTitle>Email Adress</AuthTitle>
+      <Auth>
+        <AuthContainer>
+          <AuthTitle>Full Name</AuthTitle>
 
-                        <AuthField 
-                            autoCapitalize="none"
-                            autoCompleteType="email"
-                            autoCorrect={false}
-                            autoFocus={true}
-                            keyboardType="email-address"
-                            />
-                    </AuthContainer>
+          <AuthField
+            autoCapitalize="none"
+            autoCorrect={false}
+            autoFocus={true}
+          />
+        </AuthContainer>
 
-                    <AuthContainer>
-                        <AuthTitle>Password</AuthTitle>
+        <AuthContainer>
+          <AuthTitle>Email Adress</AuthTitle>
 
-                        <AuthField 
-                            autoCapitalize="none"
-                            autoCompleteType="password"
-                            autoCorrect={false}
-                            autoFocus={true}
-                            secureTextEntry={true}
-                            />
-                    </AuthContainer>
+          <AuthField
+            autoCapitalize="none"
+            autoCompleteType="email"
+            autoCorrect={false}
+            autoFocus={true}
+            keyboardType="email-address"
+            onChangeText={text => setEmail(text)}
+          />
+        </AuthContainer>
 
-                    <AuthContainer>
-                        <AuthTitle>Confirm Password</AuthTitle>
+        <AuthContainer>
+          <AuthTitle>Password</AuthTitle>
 
-                        <AuthField 
-                            autoCapitalize="none"
-                            autoCompleteType="password"
-                            autoCorrect={false}
-                            autoFocus={true}
-                            secureTextEntry={true}
-                            />
-                    </AuthContainer>
-                </Auth>
+          <AuthField
+            autoCapitalize="none"
+            autoCompleteType="password"
+            autoCorrect={false}
+            autoFocus={true}
+            secureTextEntry={true}
+          />
+        </AuthContainer>
 
-                <SignUpContainer>
-                    <Text bold center color="#fff">Sign Up</Text>
-                </SignUpContainer>
+        <AuthContainer>
+          <AuthTitle>Confirm Password</AuthTitle>
 
-                <SignIn onPress={() => navigation.navigate("SignIn")}>
-                    <Text small center>
-                        Have an account?{" "}
-                        <Text bold color="black">
-                            Sign In
-                        </Text>
-                    </Text>
-                </SignIn>
-        </Container>
-    );
+          <AuthField
+            autoCapitalize="none"
+            autoCompleteType="password"
+            autoCorrect={false}
+            autoFocus={true}
+            secureTextEntry={true}
+          />
+        </AuthContainer>
+      </Auth>
+
+      <SignUpContainer>
+        <Text bold center color="#fff">
+          Sign Up
+        </Text>
+      </SignUpContainer>
+
+      <SignIn onPress={() => navigation.navigate('SignIn')}>
+        <Text small center>
+          Have an account?{' '}
+          <Text bold color="black">
+            Sign In
+          </Text>
+        </Text>
+      </SignIn>
+    </Container>
+  );
 };
 
 const Container = styled.View`
-    flex: 1;
+  flex: 1;
 `;
 
 const Main = styled.View`
-    margin-top: 100px;
+  margin-top: 100px;
 `;
 
 const Auth = styled.View`
-    margin: 50px 32px 32px;
+  margin: 50px 32px 32px;
 `;
 
 const AuthContainer = styled.View`
-    margin-bottom: 20px;
+  margin-bottom: 20px;
 `;
 
 const AuthTitle = styled(Text)`
-    color: #8e93a1;
-    font-size: 12px;
-    text-transform: uppercase;
-    font-weight: 300;
+  color: #8e93a1;
+  font-size: 12px;
+  text-transform: uppercase;
+  font-weight: 300;
 `;
 
 const AuthField = styled.TextInput`
-    border-bottom-color: #8e93a1;
-    border-bottom-width: 1px;
-    height: 48px; 
+  border-bottom-color: #8e93a1;
+  border-bottom-width: 1px;
+  height: 48px;
 `;
 
 const SignUpContainer = styled.TouchableOpacity`
-    margin: 0 32px;
-    height: 48px;
-    align-items: center;
-    justify-content: center;
-    background-color: #8e93a1;
-    border-radius: 6px;
+  margin: 0 32px;
+  height: 48px;
+  align-items: center;
+  justify-content: center;
+  background-color: #8e93a1;
+  border-radius: 6px;
 `;
 
 const SignIn = styled.TouchableOpacity`
-    margin-top: 16px;
+  margin-top: 16px;
 `;
