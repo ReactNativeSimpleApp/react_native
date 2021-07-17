7e1b0e09a31208db8ac106d28b3be712ecedba9a
Роман Худобляк <49787417+RKDGT@users.noreply.github.com>
2021-07-17 15:35:48 +0300

    add api request to posts + show all of them

--- src/components/stacks/MainStack.js
+++ src/components/stacks/MainStack.js
@@ -1,5 +1,5 @@
-import React from  "react";
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import Home from '../screens/Home';
@@ -7,41 +7,49 @@ import Gallery from '../screens/Gallery';
 import Profiles from '../screens/Profiles';
 
 export default MainStack = () => {
-    const MainS = createBottomTabNavigator();
-    const tabBarOptions = {
-        showLabel: false,
-        style: {
-            borderTopWidth: 1,
-            borderTopColor: "#17a2b8",
-            backgroundColor: "#222222"
-        },
-    };
+  const MainS = createBottomTabNavigator();
+  const tabBarOptions = {
+    showLabel: true,
+    style: {
+      borderTopWidth: 1,
+      borderTopColor: '#17a2b8',
+      backgroundColor: '#222222',
+    },
+  };
 
-    const screenOptions = (({route}) => ({
-        tabBarIcon: ({focused}) => {
-            let iconName = "md-home"
+  const screenOptions = ({route}) => ({
+    tabBarIcon: ({focused}) => {
+      let iconName = 'md-home';
 
-            switch(route.name) {
-                case "Home":
-                    iconName = "md-home"
-                    break;
-                case "Gallery":
-                    iconName = "md-images"
-                    break;
-                case "Profiles":
-                    iconName = "md-people"
-                    break;
-                default:
-                    iconName = "md-home"
-            }
-            return <Ionicons name={iconName} size={24} color={focused ? "#17a2b8" : "#666"}/>
-        }
-    }))
- return (
-    <MainS.Navigator screenOptions={screenOptions} tabBarOptions={tabBarOptions}>
-        <MainS.Screen name="Home" component={Home} />
-        <MainS.Screen name="Gallery" component={Gallery} />
-        <MainS.Screen name="Profiles" component={Profiles} />
+      switch (route.name) {
+        case 'Home':
+          iconName = 'md-home';
+          break;
+        case 'Gallery':
+          iconName = 'md-images';
+          break;
+        case 'Profiles':
+          iconName = 'md-people';
+          break;
+        default:
+          iconName = 'md-home';
+      }
+      return (
+        <Ionicons
+          name={iconName}
+          size={24}
+          color={focused ? '#17a2b8' : '#666'}
+        />
+      );
+    },
+  });
+  return (
+    <MainS.Navigator
+      screenOptions={screenOptions}
+      tabBarOptions={tabBarOptions}>
+      <MainS.Screen name="Home" component={Home} />
+      <MainS.Screen name="Gallery" component={Gallery} />
+      <MainS.Screen name="Profiles" component={Profiles} />
     </MainS.Navigator>
- );
+  );
 };
