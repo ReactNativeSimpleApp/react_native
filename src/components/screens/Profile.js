51450422c254a7b367e7e8de388032f509e1c611
Роман Худобляк <49787417+RKDGT@users.noreply.github.com>
2021-07-17 21:47:03 +0300

    profiles from api and indiv profs for chosen  user

--- src/components/screens/Profile.js
+++ src/components/screens/Profile.js
@@ -1,107 +1,136 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
 
-import { View } from 'react-native';
-
 import Text from '../reusable/Text';
+import jsonApiService from '../../services/jsonApiServise';
 
-const Profile = ({...props}) => {
-    return (
-        <Container>
-            <UserContainer>
-                <Text title heavy margin="16px 0 18px 0" color="#17a2b8">
-                    {/* {props.name} */}
-                    John Doe
-                </Text>
+const Profile = ({route}) => {
+  const [user, setUser] = useState([
+    {
+      name: '',
+      id: '',
+      username: '',
+      email: '',
+      phone: '',
+    },
+  ]);
+  useEffect(() => {
+    jsonApiService
+      .getUsers({id: route.params.userId})
+      .then(data => {
+        setUser(
+          data.data.data.map(user => ({
+            name: user.name,
+            username: user.username,
+            email: user.email,
+            phone: user.phone,
+            website: user.website,
+            street: user.address.street,
+            suite: user.address.suite,
+            city: user.address.city,
+            companyName: user.company.name,
+            companyCatchPhrase: user.company.catchPhrase,
+          })),
+        );
+        console.log(user);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }, []);
 
-                <Text large bold margin="0 0 8px 0" color="#8e93a1">
-                    {/* {props.username} */}
-                    johndoe
-                </Text>
+  return (
+    <Container>
+      <UserContainer>
+        <Text title heavy margin="16px 0 18px 0" color="#17a2b8">
+          {user[0].name}
+        </Text>
 
-                <Text medium bold margin="0 0 8px 0" color="#8e93a1">
-                    {/* {props.email} */}
-                    johndoe@gmail.com
-                </Text>
+        <Text large bold margin="0 0 8px 0" color="#8e93a1">
+          {user[0].username}
+        </Text>
 
-                <Text medium bold margin="0 0 8px 0" color="#8e93a1">
-                    {/* {props.phone} */}
-                    +123456789
-                </Text>
+        <Text medium bold margin="0 0 8px 0" color="#8e93a1">
+          {user[0].email}
+        </Text>
 
-                <Text medium bold margin="0 0 8px 0" color="#8e93a1">
-                    {/* {props.website} */}
-                    google.com
-                </Text>
-            </UserContainer>
-            
-            <AdressContainer>
-                <Text medium heavy color="#8e93a1" padding="4px 0">Adress:</Text>
-                <Text medium bold color="#8e93a1">
-                    {/* {props.adress.street} */}
-                    Lenina
-                </Text>
-                <Text medium bold color="#8e93a1">
-                    {/* {props.adress.suit} */}
-                    456
-                </Text>
-                <Text medium bold color="#8e93a1">
-                    {/* {props.adress.city} */}
-                    NY
-                </Text>    
-            </AdressContainer>
+        <Text medium bold margin="0 0 8px 0" color="#8e93a1">
+          {user[0].phone}
+        </Text>
 
-            <CompanyContainer>
-                <Text medium heavy color="#8e93a1" padding="4px 0">Company:</Text>
-                <Text medium bold color="#8e93a1">
-                    {/* {props.company.name} */}
-                    GOOGLE
-                </Text>
-                <Text medium bold color="#8e93a1">
-                    {/* {props.company.catchPhrase} */}
-                    lalalala lalala la
-                </Text>
-            </CompanyContainer>
+        <Text medium bold margin="0 0 8px 0" color="#8e93a1">
+          {user[0].website}
+        </Text>
+      </UserContainer>
 
-            <LogOut>
-                <Text bold center color="#17a2b8">Log Out</Text>
-            </LogOut>
-        </Container>
-    );
+      <AdressContainer>
+        <Text medium heavy color="#8e93a1" padding="4px 0">
+          Adress:
+        </Text>
+        <Text medium bold color="#8e93a1">
+          {user[0].street}
+        </Text>
+        <Text medium bold color="#8e93a1">
+          {user[0].suite}
+        </Text>
+        <Text medium bold color="#8e93a1">
+          {user[0].city}
+        </Text>
+      </AdressContainer>
+
+      <CompanyContainer>
+        <Text medium heavy color="#8e93a1" padding="4px 0">
+          Company:
+        </Text>
+        <Text medium bold color="#8e93a1">
+          {user[0].companyName}
+        </Text>
+        <Text medium bold color="#8e93a1">
+          {user[0].companyCatchPhrase}
+        </Text>
+      </CompanyContainer>
+
+      <LogOut>
+        <Text bold center color="#17a2b8">
+          Log Out
+        </Text>
+      </LogOut>
+    </Container>
+  );
 };
 
 export default Profile;
 
 const Container = styled.View`
-    flex: 1;
-    background-color: #343a40;
+  flex: 1;
+  background-color: #343a40;
 `;
 
 const UserContainer = styled.View`
-    padding: 20px 0;
-    align-items: center;
-    justify-content: center;
-    borderBottomWidth: 1px;
-    borderBottomColor: #17a2b8;
-    background-color: #222222;
+  padding: 20px 0;
+  align-items: center;
+  justify-content: center;
+  border-bottom-width: 1px;
+  border-bottom-color: #17a2b8;
+  background-color: #222222;
 `;
 
 const AdressContainer = styled.View`
-    align-items: center;
-    margin-top: 16px;
+  align-items: center;
+  margin-top: 16px;
 `;
 
 const CompanyContainer = styled.View`
-    align-items: center;
-    margin-top: 16px;
+  align-items: center;
+  margin-top: 16px;
 `;
 
 const LogOut = styled.TouchableOpacity`
-    bottom: -120px;
-    margin: 10px 32px;
-    height: 48px;
-    align-items: center;
-    justify-content: center;
-    background-color: #222222;
-    border-radius: 6px;
+  bottom: -120px;
+  margin: 10px 32px;
+  height: 48px;
+  align-items: center;
+  justify-content: center;
+  background-color: #222222;
+  border-radius: 6px;
 `;
--- src/components/screens/Profiles.js
+++ src/components/screens/Profiles.js
@@ -1,45 +1,68 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
 
-import { View, ScrollView } from 'react-native';
+import {View} from 'react-native';
 
 import Text from '../reusable/Text';
 import ProfileItem from '../reusable/ProfileItem';
 
+import jsonApiService from '../../services/jsonApiServise';
+
 export default Profiles = ({navigation}) => {
-    return (
-        <Container>
-            <View>
-                <Text title heavy margin="40px 20px 10px 20px" color="#17a2b8">
-                    Profiles:
-                </Text>
-            
-                <Profile onPress={() => navigation.navigate("Profile")}>
-                    <ProfileItem 
-                        name="John Doe"
-                        mobile="+123456789"
-                        email="johndoe@gmail.com"/> 
-                </Profile>
-
-                <ProfileItem 
-                    name="Kate Smith"
-                    mobile="+987654321"
-                    email="katesmith@gmail.com"/> 
-
-                <ProfileItem 
-                    name="James Brown"
-                    mobile="+987654721"
-                    email="jamesbrown@gmail.com"/> 
-            </View>
-        </Container>
-    );
-}
+  const [users, setUsers] = useState([
+    {
+      name: '',
+      id: '',
+      username: '',
+      email: '',
+      phone: '',
+    },
+  ]);
+  useEffect(() => {
+    jsonApiService
+      .getUsers()
+      .then(data => {
+        setUsers(
+          data.data.data.map(user => ({
+            name: user.name,
+            id: user.id,
+            username: user.username,
+            email: user.email,
+            phone: user.phone,
+          })),
+        );
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }, []);
+  return (
+    <Container>
+      <View>
+        <Text title heavy margin="40px 20px 10px 20px" color="#17a2b8">
+          Profiles:
+        </Text>
+        {users.map(user => (
+          <Profile
+            onPress={() => navigation.navigate('Profile', {userId: user.id})}
+            key={user.id}>
+            <ProfileItem
+              name={`${user.name}, ${user.username}`}
+              mobile={user.phone}
+              email={user.email}
+            />
+          </Profile>
+        ))}
+      </View>
+    </Container>
+  );
+};
 
 const Container = styled.ScrollView`
-    flex: 1;
-    background-color: #343a40;
+  flex: 1;
+  background-color: #343a40;
 `;
 
 const Profile = styled.TouchableOpacity`
-    margin-top: 16px;
+  margin-top: 16px;
 `;
