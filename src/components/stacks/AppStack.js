c2716b86c5df3479e79a8ca5026dd633c91ac3a3
Роман Худобляк <49787417+RKDGT@users.noreply.github.com>
2021-07-19 16:19:01 +0300

    іф

--- src/components/stacks/AppStack.js
+++ src/components/stacks/AppStack.js
@@ -1,32 +1,27 @@
-import React, { useContext } from 'react';
+import React from 'react';
 
 import {createStackNavigator} from '@react-navigation/stack';
 
-import UserContext from '../../context/UserContext';
-
 import AuthStack from './AuthStack';
 import MainStack from './MainStack';
-import Profiles from '../screens/Profiles';
 import Profile from '../screens/Profile';
 import Post from '../screens/Post';
-import Gallery from '../screens/Gallery';
 
 export default AppStack = () => {
-    const AppStackNav = createStackNavigator();
-    const user = {
-        isLoggedIn: false
-    };
+  const AppStackNav = createStackNavigator();
+  const user = {
+    isLoggedIn: false,
+  };
 
-    return (
-        <AppStackNav.Navigator headerMode="none">
-            {user.isLoggedIn ? (
-                <AppStackNav.Screen name="Main" component={MainStack} />
-            ) : (
-                <AppStackNav.Screen name="Auth" component={AuthStack} />
-            )}
-                <AppStackNav.Screen name="Profile" component={Profile} />
-                <AppStackNav.Screen name="Post" component={Post} />
-
-        </AppStackNav.Navigator>
-    );
-}
+  return (
+    <AppStackNav.Navigator headerMode="none">
+      {user.isLoggedIn ? (
+        <AppStackNav.Screen name="Main" component={MainStack} />
+      ) : (
+        <AppStackNav.Screen name="Auth" component={AuthStack} />
+      )}
+      <AppStackNav.Screen name="Profile" component={Profile} />
+      <AppStackNav.Screen name="Post" component={Post} />
+    </AppStackNav.Navigator>
+  );
+};
